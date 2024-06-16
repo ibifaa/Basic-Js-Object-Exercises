4463676a76063b93add4d42887439904206d691e
ibifaa ibisaki aruoture <ibifaaibisakiaruoture@gmail.com>
2024-06-16 07:35:56 +0100

    exercise 5 completed

--- exercise1.js
+++ exercise1.js
@@ -55,3 +55,29 @@ function listProperties(book){
 }
 
 console.log(listProperties(book))
+
+// Exercise 5
+
+const library={
+    name:"",
+    books:[{
+        title:"",
+        year:0,
+        author:"",
+        pages:0
+    }],
+}
+
+function addBook(library, newBook){
+    const {name, books}= library
+    books.push(newBook)
+    console.log(books)
+
+}
+
+addBook(library, {
+    title:"The Lion and the Unicorn",
+    year:1984,
+    author:"George Orwell",
+    pages:328
+})
