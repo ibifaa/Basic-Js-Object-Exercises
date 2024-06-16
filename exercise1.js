569570535f5a9f6a3b2912ef1b94d59dea555239
ibifaa ibisaki aruoture <ibifaaibisakiaruoture@gmail.com>
2024-06-16 06:33:56 +0100

    exercise1completed

---
+++ exercise1.js
@@ -0,0 +1,12 @@
+const person ={
+    firstName: "John",
+    lastName: "Doe",
+    age:30,
+    city:"New York"
+}
+
+function introduce(person){
+return (`Hello, my name is ${person.firstName} ${person.lastName}. I am ${person.age} and i live in ${person.city}`)
+}
+
+console.log(introduce(person))
