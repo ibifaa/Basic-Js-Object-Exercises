dcee0fdf8e8d09e596aaa1e112aa346cf9e37a5d
ibifaa ibisaki aruoture <ibifaaibisakiaruoture@gmail.com>
2024-06-16 07:14:35 +0100

    exercise 4 completed

--- exercise1.js
+++ exercise1.js
@@ -38,3 +38,20 @@ function removeProperty(car, property) {
 
 removeProperty(car, car.color)
 console.log(car)
+
+// Exercise 4
+const book ={
+    tile:"Mark",
+    author:"",
+    pages:"",
+    publisher:"",
+}
+
+function listProperties(book){
+    for(const props in book){
+   
+        console.log(props)
+    }
+}
+
+console.log(listProperties(book))
